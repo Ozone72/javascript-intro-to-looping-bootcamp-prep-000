1f95fb0089413715e619174f6d974bd68dad274e
Orin Fletcher <orin.fletcher@gmail.com>
2017-10-04 21:17:58 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,31 @@
+function forLoop(array){
+  for (let i = 0; i < 25; i++) {
+    if (i === 1){
+      array.push("I am 1 strange loop.")
+    }
+    else{
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array
+}
+
+//console.log(forLoop([]));
+function whileLoop(n){
+  while(n > 0){
+    console.log(n--)
+  }
+  return "done"
+}
+
+//whileLoop(10)
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array){
+  do {
+    array.pop();
+  } while (array.length > 0 && maybeTrue()); //array greater than 0 and maybeTrue<.5
+  return array;
+}
