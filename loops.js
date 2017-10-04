bc4bb2e7b47b4e80b564b0d613d87b8cb85f17a8
Orin Fletcher <orin.fletcher@gmail.com>
2017-10-04 20:51:15 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,6 @@
 function forLoop(array){
   for (let i = 0; i < 25; i++) {
-    let array = array
-    array.push[`I am ${i} strange loop.`]
+    array.push(`I am ${i} strange loop.`)
     return array
   }
 }
